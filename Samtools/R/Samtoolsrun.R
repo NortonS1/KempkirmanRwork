befd5373dd2b102956657aab8f0122f01657262f
Kemp Lab Admin <kemplabadmin@micro1109.otago.ac.nz>
2016-11-14 10:22:30 +1300

    inital

---
+++ Samtools/R/Samtoolsrun.R
@@ -0,0 +1,7 @@
+#' This is a test
+
+Samtoolsrun <- function(run){
+    if(run == "run"){
+      shiny::runApp(appDir = getwd())
+    }
+  }
---
+++ tempconvert/R/tempconvert.R
@@ -0,0 +1,6 @@
+#'This is a test
+
+fahr_to_kelvin <- function(temp){
+  kelvin <- (temp-1)
+  return(kelvin)
+}
