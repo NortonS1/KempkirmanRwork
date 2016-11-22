5c109e9e2fb56a4e0e20e71520ab9acdf90097e0
NortonS1 <sam.norton11@hotmail.co.uk>
2016-11-23 08:54:09 +1300

    Refined SPADE

--- PUT FILES IN HERE/SPADE.R
+++ PUT FILES IN HERE/SPADE.R
@@ -2,26 +2,29 @@
 
 read.csv("Sample 1.csv") -> x
 library(igraph)
-SPADE <- function(x,k){
 
+SPADE <- function(x,k){
+  
+  #initial clustering and binning 
 dist(x, method = "manhattan") -> distx
 hclust(distx) -> clus_x
 cutree(clus_x, k = k) ->cut_x
-
-
 datalist = list()
 for(i in 1:k){
 dat = data.frame(colMeans(x[c(cut_x == i),]))
 datalist[[i]] <- dat
 }
+#cleaning data and assigning to data frame
 big_data = do.call(cbind, datalist)
 big_data = t(big_data)
-dist(big_data, method = "manhattan") -> distx1
-#final graphing
-graph.adjacency(as.matrix(distx1),mode="directed",weighted=TRUE) -> adjgraph
-minimum.spanning.tree(adjgraph) -> SPADEgraph
-plot(SPADEgraph)
+clus_num <- c(1:k)
+clus_names <- as.character(clus_num)
+as.data.frame(big_data, row.names = c(clus_names)) -> cluster_means
 
-return(big_data)
+#calculating cluster distances and plotting
+dist(cluster_means, method = "manhattan") -> distx1
+graph.adjacency(as.matrix(distx1),mode="undirected",weighted=TRUE) -> adjgraph
+minimum.spanning.tree(adjgraph) -> SPADEgraph
+tkplot(SPADEgraph)
 }
 
